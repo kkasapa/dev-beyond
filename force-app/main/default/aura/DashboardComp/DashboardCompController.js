7956ee4e95bb717011352808fa21f18ea7cdad44
VigneshSadhasivam <akannan@selastech.com>
2019-09-10 13:47:33 +0530

    Initial Commit Day 10-09-2019

--- force-app/main/default/aura/DashboardComp/DashboardCompController.js
+++ force-app/main/default/aura/DashboardComp/DashboardCompController.js
@@ -1,6 +1,7 @@
 ({
     doInit : function(component, event, helper) {
         component.set('v.showSpinner', true);
+        component.set('v.data',[]);
         helper.setColumns(component);
         console.log('recordId',component.get('v.programId'));
         const apexService = component.find("apexService");
@@ -15,6 +16,31 @@
             }),
             $A.getCallback(function(response) { // Error callback
                 component.set('v.showSpinner', false);
+                console.log('Error',response);
+            })
+        );
+    },
+    handleActive: function (cmp, event, helper) {
+        console.log('injecting comp');
+        helper.handleActive(cmp, event);
+    },
+    getNewSettlements: function (cmp, event, helper) {
+        console.log('onActive');
+        cmp.set('v.showSpinner', true);
+        cmp.set('v.data',[]);
+        const apexService = cmp.find("apexService");
+		apexService.callServer(
+            cmp.get("c.getNewSettlementsfromTradeline"), // Action
+            {"progId": cmp.get('v.programId')}, // Action parameters 'a0L22000000Vmzh'
+            $A.getCallback(function(response) { // Success callback
+                var tradelineList = JSON.parse(response); 
+                console.log('tradelineList-->',tradelineList);
+                cmp.set('v.data',tradelineList);
+                helper.setTradelinesColumns(cmp);
+                cmp.set('v.showSpinner', false);
+            }),
+            $A.getCallback(function(response) { // Error callback
+                cmp.set('v.showSpinner', false);
                 console.log('Error',response);
             })
         );
--- force-app/main/default/aura/DashboardComp/DashboardCompHelper.js
+++ force-app/main/default/aura/DashboardComp/DashboardCompHelper.js
@@ -18,5 +18,52 @@
             {label: 'Payment Amount', fieldName: 'paymentAmount', type: 'currency', typeAttributes: { currencyCode: 'USD', maximumSignificantDigits: 5}, sortable: true},
             {label: 'Payment Total Method', fieldName: 'paymentTotalAmount', type: 'currency', typeAttributes: { currencyCode: 'USD', maximumSignificantDigits: 5}, sortable: true}
         ]);
+    },
+    setTradelinesColumns : function(cmp) {
+        console.log('setting columns');
+        cmp.set('v.columns', [
+            {label: 'Tradeline Name', fieldName: 'tradelineName', type: 'text', sortable: true},
+            {label: 'Offer Name', fieldName: 'offerName', type: 'text', sortable: true},
+            {label: 'Offer Amount', fieldName: 'offerAmount', type: 'currency', typeAttributes: { currencyCode: 'USD', maximumSignificantDigits: 5}},
+            {label: 'Payment Name', fieldName: 'paymentName', type: 'text', sortable: true},
+            {label: 'Payment Sequence Number', fieldName: 'paymentSequenceNumber', type: 'integer', sortable: true},
+            {label: 'Schedule Date', fieldName: 'scheduleDate', type: 'date', sortable: true},
+            {label: 'Payment Amount', fieldName: 'paymentAmount', type: 'currency', typeAttributes: { currencyCode: 'USD', maximumSignificantDigits: 5}, sortable: true},
+            {label: 'Payment Total Method', fieldName: 'paymentTotalAmount', type: 'currency', typeAttributes: { currencyCode: 'USD', maximumSignificantDigits: 5}, sortable: true}
+        ]);
+    },
+    handleActive: function (cmp, event) {
+        var tab = event.getSource();
+        console.log('ids->'+tab.get('v.id'));
+        switch (tab.get('v.id')) {
+            case 'tradelineSettlement' :
+                this.injectComponent(cmp, 'c:RelatedListTableComp', tab);
+                break;
+            case 'tradelineSettlementsPending' :
+                this.injectComponent(cmp,'c:RelatedListTableComp', tab);
+                break;
+            case 'otherTradelines':
+                this.injectComponent(cmp, 'c:RelatedListTableComp', tab);
+                break;
+            case 'tradelineSettlementPayments':
+                this.injectComponent(cmp, 'c:RelatedListTableComp', tab);
+                break;
+        }
+    },
+    injectComponent: function (cmp, name, target) {
+        console.log('injecting');
+        let data = cmp.get('v.data');
+        $A.createComponent(name, {
+            "data" : data,
+            "column" : cmp.get("v.columns"),
+            "iconName" : "standard:individual", 
+            "cardTitle" : "Tradelines/Creditors" 
+        }, function (contentComponent, status, error) {
+            if (status === "SUCCESS") {
+                target.set('v.body', contentComponent);
+            } else {
+                throw new Error(error);
+            }
+        });
     }
 })
