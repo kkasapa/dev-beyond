4a15a17b28ee54d7b723c019f96b7326338742e6
Ashwin Bretto <abrettoss@gmail.com>
2019-10-23 15:15:34 +0530

    initial commit 11/11

---
+++ force-app/main/default/aura/OfferSection/OfferSectionController.js
@@ -0,0 +1,64 @@
+({        
+    doInit: function(component, event, helper) {          
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)); //You get the whole decoded URL of the page.
+        var sURLVariables = sPageURL.split('&'); //Split by & so that you get the key value pairs separately in a list
+        var sParameterName;
+        var i;
+        
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('='); //to split the key from the value.
+            
+            if (sParameterName[0] === 'id') { 
+                sParameterName[1] === undefined ? 'Not found' : sParameterName[1];
+            }
+        }        
+        var finalvalue = sParameterName[1];
+        component.set("v.parameters", finalvalue);
+        
+        helper.getTLines(component, event, helper);
+        helper.getOfferPicklist(component, event, helper);
+        helper.getPaymentMethod(component,event,helper);
+        helper.getPgm(component,event,helper);
+        
+    },   
+    
+    getPays : function(component, event, helper) {
+        helper.getPayments(component, event, helper);
+    },
+    
+    toggleSection : function(component, event, helper) { 
+        // dynamically get aura:id name from 'data-auraId' attribute
+        var sectionAuraId = event.target.getAttribute("data-auraId");
+        // get section Div element using aura:id
+        var sectionDiv = component.find(sectionAuraId).getElement();
+        /* The search() method searches for 'slds-is-open' class, and returns the position of the match.
+         * This method returns -1 if no match is found.
+        */
+        var sectionState = sectionDiv.getAttribute('class').search('slds-is-open'); 
+        
+        // -1 if 'slds-is-open' class is missing...then set 'slds-is-open' class else set slds-is-close class to element
+        if(sectionState == -1){
+            sectionDiv.setAttribute('class' , 'slds-section slds-is-open');
+        }else{
+            sectionDiv.setAttribute('class' , 'slds-section slds-is-close');
+        }       
+    },
+    
+    toggleClientSection : function(component, event, helper) { 
+        // dynamically get aura:id name from 'data-auraId' attribute
+        var sectionAuraId = event.target.getAttribute("data-auraId");
+        // get section Div element using aura:id
+        var sectionDiv = component.find(sectionAuraId).getElement();
+        /* The search() method searches for 'slds-is-open' class, and returns the position of the match.
+         * This method returns -1 if no match is found.
+        */
+        var sectionState = sectionDiv.getAttribute('class').search('slds-is-open'); 
+        
+        // -1 if 'slds-is-open' class is missing...then set 'slds-is-open' class else set slds-is-close class to element
+        if(sectionState == -1){
+            sectionDiv.setAttribute('class' , 'slds-section slds-is-open');
+        }else{
+            sectionDiv.setAttribute('class' , 'slds-section slds-is-close');
+        }       
+    }
+})
